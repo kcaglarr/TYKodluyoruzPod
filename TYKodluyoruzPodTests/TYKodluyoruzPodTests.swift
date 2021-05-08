19cadca9319e727b346e016295e796bda066e65e
kerimcaglar <kerim.caglarr@gmail.com>
2021-05-08 12:39:33 +0300

    Added tests

--- TYKodluyoruzPodTests/TYKodluyoruzPodTests.swift
+++ TYKodluyoruzPodTests/TYKodluyoruzPodTests.swift
@@ -10,24 +10,19 @@ import XCTest
 
 class TYKodluyoruzPodTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var tyKodluyoruzPod : TYKodluyoruzPod!
+    
+    override func setUp() {
+        tyKodluyoruzPod = TYKodluyoruzPod()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testSum() {
+        XCTAssertEqual(tyKodluyoruzPod.sum(x: 3, y: 5), 8)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testMultiply() {
+        XCTAssertEqual(tyKodluyoruzPod.multiply(x: 3, y: 5), 15)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
